3a17288cf98d7a038b717bd5589f92eec92874d3
Maria de Fátima <mnunesalves334@gmail.com>
2025-07-08 15:44:58 -0300

    chore: Adicionar rota da tela de login e estrutura do react-router-dom

--- desafio-frontend-api-ecommerce-parte-2-maria-daniely/src/App.tsx
+++ desafio-frontend-api-ecommerce-parte-2-maria-daniely/src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from "../src/components/Login/Login"
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello Tailwind!
-    </h1>
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </>
   )
 }
 
