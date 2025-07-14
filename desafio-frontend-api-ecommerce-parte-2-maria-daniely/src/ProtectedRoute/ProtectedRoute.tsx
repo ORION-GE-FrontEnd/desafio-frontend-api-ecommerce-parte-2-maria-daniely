2e43c08a391a083fe2304e578659936bf5f02b44
Maria de Fátima <mnunesalves334@gmail.com>
2025-07-14 13:15:50 -0300

    feat(auth): adiciona lógica de rotas privadas e redirecionamento para login

---
+++ desafio-frontend-api-ecommerce-parte-2-maria-daniely/src/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../components/context/useAuth';
+import type { JSX } from 'react/jsx-runtime';
+
+export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
