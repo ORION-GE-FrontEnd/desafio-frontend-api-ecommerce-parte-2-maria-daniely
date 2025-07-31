42d0ca107c4f060ccb035be586ef398905f080fe
DanielyVasconcelos2024 <daniely.vasconcelos@academico.uncisal.edu.br>
2025-07-30 22:36:59 -0300

    chore: reconfigura setup do Cypress

--- desafio-frontend-api-ecommerce-parte-2-maria-daniely/cypress/e2e/testes-compras.cy.ts
+++ desafio-frontend-api-ecommerce-parte-2-maria-daniely/cypress/e2e/testes-compras.cy.ts
@@ -1,9 +1,9 @@
 /// <reference types="cypress" />
 
-// Dados de teste para o usuário
+// Dados do usuário que será usado para testar a loja.
 const usuarioDeTeste = {
   nome: 'Daniely Vasconcelos',
-  email: 'daniely.vasconcelos@test.com',
+  email: 'danielyvasconcelos@gmail.com',
   password: 'minhasenha123'
 };
 
@@ -22,116 +22,73 @@ const garantirUsuarioSalvo = () => {
 
 // Realiza o processo de login
 const fazerLogin = () => {
-  garantirUsuarioSalvo(); 
-  cy.visit('/login');
-  cy.get('input[placeholder="E-mail"]').type(usuarioDeTeste.email);
-  cy.get('input[placeholder="Senha"]').type(usuarioDeTeste.password);
-  cy.get('button[type="submit"]').contains('Login').click();
-  cy.url().should('include', '/home');
-  cy.contains(`Bem-vindo(a), ${usuarioDeTeste.nome}`).should('be.visible');
-  cy.log('Login Concluído.');
+  cy.visit('/login'); 
+  cy.get('input[placeholder="E-mail"]').type(usuarioDeTeste.email); 
+  cy.get('input[placeholder="Senha"]').type(usuarioDeTeste.password); 
+  cy.get('button[type="submit"]').contains('Login').click(); 
+  cy.url().should('include', '/home'); 
+  cy.contains(`Bem-vindo(a), ${usuarioDeTeste.nome}`).should('be.visible'); 
+  cy.log('Login feito com sucesso.'); 
 };
 
-// Adiciona um produto ao carrinho e navega para a página do carrinho
+//  Adiciona um produto ao carrinho e navega para a página do carrinho.
 const adicionarUmProdutoEClicarCarrinho = () => {
-  cy.contains('Todos os Produtos').should('be.visible');
-  cy.scrollTo('bottom', { duration: 1000 });
-  cy.wait(1000);
+  cy.contains('Todos os Produtos').should('be.visible'); 
+  cy.scrollTo('bottom', { duration: 1000 }); 
+  cy.wait(1000); 
+
   cy.get('button.bg-gray-800').contains('Adicionar ao carrinho').first().should('be.visible').click();
   cy.wait(500);
-  cy.get('button[aria-label="Ir para o carrinho de compras"]').click();
-  cy.url().should('include', '/carrinho');
-  cy.contains('Carrinho de Compras').should('be.visible');
-  cy.get('.flex.justify-between.items-center.bg-amber-50').should('have.length', 1);
-  cy.log('Um item adicionado e carrinho verificado.');
+  cy.get('button[aria-label="Ir para o carrinho de compras"]').click(); 
+  cy.url().should('include', '/carrinho'); 
+  cy.contains('Carrinho de Compras').should('be.visible'); 
+  cy.get('.flex.justify-between.items-center.bg-amber-50').should('have.length', 1); 
+  cy.log('Um produto adicionado ao carrinho.'); 
 };
 
 // Preenche e submete o formulário de pagamento
 const preencherFormularioPagamento = () => {
   cy.get('button').contains('Finalizar Compra').click(); 
-  cy.url().should('include', '/pagamento');
-  cy.contains('Forma de Pagamento').should('be.visible');
+  cy.url().should('include', '/pagamento'); 
+  cy.contains('Forma de Pagamento').should('be.visible'); 
 
-  cy.get('input[type="radio"][value="pix"]').click({ force: true });
-  cy.contains('Você escolheu a forma pix de pagamento.').should('be.visible');
+  cy.get('input[type="radio"][value="pix"]').click({ force: true }); 
+  cy.contains('Você escolheu a forma pix de pagamento.').should('be.visible'); 
 
-  cy.get('input[placeholder="Endereço"]').type('Villa Kenedy Quadra B');
-  cy.get('input[placeholder="Número/Complemento"]').type('17');
-  cy.get('button[type="submit"]').contains('Confirmar Pedido').click();
+  cy.get('input[placeholder="Endereço"]').type('Villa Kenedy Quadra B'); 
+  cy.get('input[placeholder="Número/Complemento"]').type('17'); 
+  cy.get('button[type="submit"]').contains('Confirmar Pedido').click(); 
   
   cy.url().should('include', '/resumo-pedido');
   cy.contains('Resumo do Pagamento').should('be.visible');
-  cy.log('Formulário de pagamento preenchido e resumo verificado.');
+  cy.log('Formulário de pagamento preenchido e resumo verificado.'); 
 };
 
+
 //Blocos de Teste 
-
-describe('Login de Usuário', () => {
-  beforeEach(() => {
-    cy.clearLocalStorage();
-    garantirUsuarioSalvo(); 
-    cy.viewport(1280, 800);
-    cy.visit('/login');
-  });
-
-  it('Deve permitir ao usuário fazer login com credenciais válidas', () => {
-    cy.get('input[placeholder="E-mail"]').type(usuarioDeTeste.email);
-    cy.get('input[placeholder="Senha"]').type(usuarioDeTeste.password);
-    cy.get('button[type="submit"]').contains('Login').click();
-    cy.url().should('include', '/home');
-    cy.contains(`Bem-vindo(a), ${usuarioDeTeste.nome}`).should('be.visible');
-  });
+before(() => {
+  cy.clearLocalStorage();
+  cy.viewport(1280, 800); 
+  garantirUsuarioSalvo(); 
 });
 
-describe('Adicionar Item ao Carrinho', () => {
-  beforeEach(() => {
-    cy.clearLocalStorage();
-    garantirUsuarioSalvo(); 
-    cy.viewport(1280, 800);
-    fazerLogin();
-  });
-
-  it('Deve permitir adicionar um produto ao carrinho e verificar na página do carrinho', () => {
-    adicionarUmProdutoEClicarCarrinho(); 
-  });
+beforeEach(() => {
+  cy.clearLocalStorage('carrinhoItens'); 
+  cy.clearLocalStorage('paymentDetails'); 
+  
 });
 
-describe('Checkout do Pedido', () => {
-  beforeEach(() => {
-    cy.clearLocalStorage();
-    garantirUsuarioSalvo(); 
-    cy.viewport(1280, 800);
-    fazerLogin(); 
-    adicionarUmProdutoEClicarCarrinho(); 
-  });
-
-  it('Deve permitir preencher o formulário de pagamento e navegar para o resumo do pedido', () => {
-    preencherFormularioPagamento(); 
-  });
-});
-
-describe('Confirmação Final do Pedido', () => {
-  beforeEach(() => {
-    cy.clearLocalStorage();
-    garantirUsuarioSalvo(); 
-    cy.viewport(1280, 800);
+describe('Teste do Fluxo Completo de Compra na OrionStore', () => {
+  it('Deve permitir login, adicionar um produto, passar pelo checkout e confirmar o pedido', () => {
     fazerLogin(); 
     adicionarUmProdutoEClicarCarrinho(); 
     preencherFormularioPagamento(); 
-  });
 
-  it('Deve permitir finalizar o pedido e exibir a confirmação na tela de resumo', () => {
     cy.get('button').contains('Finalizar Pedido').click(); 
 
-   
-    cy.contains('Pedido realizado com sucesso!').should('be.visible');
-    cy.contains('Número do Pedido: #').should('be.visible');
-    cy.contains('Obrigado por sua compra!').should('be.visible');
+    cy.contains('Pedido realizado com sucesso!').should('be.visible'); 
+    cy.contains('Número do Pedido: #').should('be.visible'); 
+    cy.contains('Obrigado por sua compra!').should('be.visible'); 
 
-  
-    cy.get('button').contains('Voltar para o Início').click();
-    cy.url().should('include', '/home');
-    cy.contains('Todos os Produtos').should('be.visible');
-    cy.log('Confirmação do pedido verificada e retorno à Home.');
   });
 });
